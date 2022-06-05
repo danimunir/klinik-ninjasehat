717c809629adb2afd6cec561fe992891063113f9
Dhani Munir Supriyadi <danimmunir1@gmail.com>
2022-06-06 01:19:17 +0700

    Add all my files

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,16 @@
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+  </div>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Selamat Datang <?= isset($user['username']) ? $user['username'] : ''; ?>
+      </h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
+</main>
+
+    
+
+  
